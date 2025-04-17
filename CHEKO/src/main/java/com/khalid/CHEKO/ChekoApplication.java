fc27fa1e3aea60b5f5ac708b52631017adee322b
khalid <10abha10@gmail.com>
2025-04-17 10:32:50 +0300

    Initial commit

---
+++ CHEKO/src/main/java/com/khalid/CHEKO/ChekoApplication.java
@@ -0,0 +1,13 @@
+package com.khalid.CHEKO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChekoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChekoApplication.class, args);
+	}
+
+}
---
+++ CHEKO/src/test/java/com/khalid/CHEKO/ChekoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khalid.CHEKO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChekoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
