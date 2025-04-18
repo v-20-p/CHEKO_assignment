ff92d2ccf426c8a92b5796b09247dacca46322ec
Khalid <ixv.pxi@gmail.com>
2025-04-18 16:52:51 +0300

    adding Dockerfiles

--- CHEKO/src/main/java/com/khalid/CHEKO/ChekoApplication.java
+++ CHEKO/src/main/java/com/khalid/CHEKO/ChekoApplication.java
@@ -1,5 +1,6 @@
 package com.khalid.CHEKO;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ChekoApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.load();
+
+		System.setProperty("spring.datasource.url", dotenv.get("SPRING_DATASOURCE_URL"));
+		System.setProperty("spring.datasource.username", dotenv.get("SPRING_DATASOURCE_USERNAME"));
+		System.setProperty("spring.datasource.password", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+
+
 		SpringApplication.run(ChekoApplication.class, args);
 	}
 
