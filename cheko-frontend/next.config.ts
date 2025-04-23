39624887d090e9c7a23e1c975bbff59eddece432
khalid <10abha10@gmail.com>
2025-04-23 09:11:02 +0300

    frontend : config for frontend (theme,redux,map,axios)

--- cheko-frontend/next.config.ts
+++ cheko-frontend/next.config.ts
@@ -1,7 +1,24 @@
-import type { NextConfig } from "next";
+// next.config.js
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig: import('next').NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/home',
+        permanent: true, 
+      },
+    ];
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
